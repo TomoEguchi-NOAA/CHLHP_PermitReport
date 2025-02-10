6eadb2e80d4da44e45de6ed991fdc43175847a49
Tomo Eguchi <Tomo.Eguchi@nmfs.local>
2025-02-10 13:07:32 -0800

    Started UAS counts but no raw data yet

--- extract_take_2024.R
+++ extract_take_2024.R
@@ -9,12 +9,17 @@ library(readxl)
 
 source("CHLHP_PermitReport_fcns.R")
 
-data.Jan_Jun <- read_xlsx(path = "data/ALEXAK_Field Data 2024-01-09 to 2024-06-13.xlsx") %>%
+# Get data for 2024-01-01 to 2024-06-30. Data for July onwards are
+# in the small boat database
+data.Jan_Jun <- read_xlsx(path = "data/ALEXAK_Field Data.xlsx") %>%
   select(Date, Time, Lat, Long, Species,
          "GroupSizeBest", "CalvesPresent",
          "UAS_GroupSize") %>%
-  filter(!is.na(Species))
+  filter(!is.na(Species)) %>%
+  filter(Date < as.Date("2024-07-01"),
+         Date > as.Date("2023-12-31"))
 
+# Create a look up table for species code and common names
 Spp.table <- data.frame(Species = c("E rob", 
                                     "G gri", 
                                     "T tru", 
@@ -28,26 +33,40 @@ Spp.table <- data.frame(Species = c("E rob",
                                        "Blue whale",
                                        "Humpback whale"))
 
+# combine the data and species table and summarize how many were sighted
+# Combine also the date and time
 data.Jan_Jun %>%
-  left_join(Spp.table) %>%
-  group_by(CommonName) %>%
+  left_join(Spp.table, by = "Species")  %>%
   select(-c("Species")) %>%
+  mutate(hr = floor(Time/100),
+         min = Time - (floor(Time/100)*100),
+         new.time = paste(hr, ifelse(min<10, paste0("0", min), min), sep = ":"))  %>% 
+  mutate(Date_Time = as.POSIXct(paste(Date, new.time, sep = " "))) %>%
+  select(-c("Date", "Time", "hr", "min", "new.time")) -> data.Jan_Jun.all
+
+data.Jan_Jun.all %>%
+  group_by(CommonName) %>%
   summarize(Best = sum(GroupSizeBest, na.rm = T),
             Best.calves = NA,
             n.sightings = n()) -> data.Jan_Jun.summary 
 
+# Extract the same information from the small boat database
 data.Jun_Dec <- extract_take_data(2024, save.file = T)
 
 # sightings
 data.Jan_Jun.summary %>%
-  full_join(data.Jun_Dec$sightings, by = "CommonName") %>%
+  full_join(data.Jun_Dec$sightings.summary, 
+            by = "CommonName") %>%
   transmute(CommonName = CommonName,
             Best = ifelse(is.na(Best.x), 0, Best.x) + 
               ifelse(is.na(Best.y), 0, Best.y),
-            Best.calves = ifelse(is.na(Best.calves.x), 0, Best.calves.x) + 
+            Best.calves = ifelse(is.na(Best.calves.x), 0, 
+                                 Best.calves.x) + 
               ifelse(is.na(Best.calves.y), 0, Best.calves.y),
-            n.sightings = ifelse(is.na(n.sightings.x), 0, n.sightings.x) + 
-              ifelse(is.na(n.sightings.y), 0, n.sightings.y)) -> summary.all
+            n.sightings = ifelse(is.na(n.sightings.x), 0, 
+                                 n.sightings.x) + 
+              ifelse(is.na(n.sightings.y), 0, 
+                     n.sightings.y)) -> summary.all
 
 write.csv(summary.all, 
           file = "data/sightings_take_numbers.csv")
@@ -55,3 +74,8 @@ write.csv(data.Jun_Dec$sightings.data,
           file = "data/sightings_Jun_Dec.csv")
 
 # UAS flights
+data.Jan_Jun.all %>%
+  group_by(CommonName) %>%
+  summarize(Best = sum(UAS_GroupSize, na.rm = T),
+            Best.calves = NA,
+            n.sightings = n()) -> data.Jan_Jun.UAS.summary 
