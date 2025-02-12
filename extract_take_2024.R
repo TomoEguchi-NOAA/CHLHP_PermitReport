974d2cffbf5e337709f91d8682bbbacb123cbc1f
Tomo Eguchi <Tomo.Eguchi@nmfs.local>
2025-02-12 15:03:05 -0800

    Adding data from manually recorded UAS fly overs.

--- extract_take_2024.R
+++ extract_take_2024.R
@@ -25,13 +25,15 @@ Spp.table <- data.frame(Species = c("E rob",
                                     "T tru", 
                                     "D cap", 
                                     "B mus", 
-                                    "M nov"),
+                                    "M nov",
+                                    "Z cal"),
                         CommonName = c("Gray whale",
                                        "Risso's dolphin",
                                        "Bottlenose dolphin",
                                        "Long-beaked common dolphin",
                                        "Blue whale",
-                                       "Humpback whale"))
+                                       "Humpback whale",
+                                       "California sea lion"))
 
 # combine the data and species table and summarize how many were sighted
 # Combine also the date and time
@@ -69,13 +71,32 @@ data.Jan_Jun.summary %>%
                      n.sightings.y)) -> summary.all
 
 write.csv(summary.all, 
-          file = "data/sightings_take_numbers.csv")
+          file = paste0("data/sightings_take_numbers_", Sys.Date(), ".csv"))
 write.csv(data.Jun_Dec$sightings.data,
-          file = "data/sightings_Jun_Dec.csv")
+          file = paste0("data/sightings_Jun_Dec_", Sys.Date(), ".csv"))
 
 # UAS flights
+# Jan flights are in a separate summarized file
+Jan.flights <- read_xlsx("data/2024_CI_UAS animal totals.xlsx") %>%
+  left_join(Spp.table, by = "Species") %>%
+  group_by(CommonName) %>%
+  summarize(Best = sum(n),
+            #Best.calves = NA,
+            n.sightings = n())
+
 data.Jan_Jun.all %>%
+  filter(Date_Time > as.Date("2024-01-31 23:59:59")) %>%
   group_by(CommonName) %>%
   summarize(Best = sum(UAS_GroupSize, na.rm = T),
-            Best.calves = NA,
-            n.sightings = n()) -> data.Jan_Jun.UAS.summary 
+            #Best.calves = NA,
+            n.sightings = n()) -> data.Feb_Jun.UAS.summary 
+
+Jan.flights %>% 
+  full_join(data.Feb_Jun.UAS.summary, 
+                          by = "CommonName") %>% #-> tmp
+  replace_na(replace = list(Best.x = 0, Best.y = 0,
+                            #Best.calves.x = 0, Best.calves.y = 0,
+                            n.sightings.x = 0, n.sightings.y = 0)) %>% # -> tmp
+  transmute(CommonName = CommonName, 
+            Best = Best.x + Best.y,
+            n.sightings = n.sightings.x + n.sightings.y) -> data.Jan_Jun.UAS.summary
